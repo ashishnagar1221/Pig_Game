306b578a36225b036286e78353cd7b2558253326
ashishnagar1221 <ashishnagar1221@gmail.com>
2019-10-12 23:50:35 +0530

    Update
    New feature have been added
    --- Winner can be decided for score upto 20

--- app.js
+++ app.js
@@ -43,23 +43,48 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
         
     }else{
         //Next player
-        //document.querySelector('#score-'+activePlayer).textContent = roundScore;
-        
-        activePlayer=== 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
-
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-        document.querySelector('.dice').style.display = 'none';
+        nextPlayer();
     }
 
 
 });
 
 
+document.querySelector('.btn-hold').addEventListener('click',function(){
+    //Add current score to global score
+    scores[activePlayer] += roundScore;
+    document.querySelector('#score-'+activePlayer).textContent = scores[activePlayer];
+
+    // Update the UI
+
+
+    //Check if player win the game
+    if(scores[activePlayer]>=20) {
+        document.querySelector('#name-'+activePlayer).textContent = 'Winner !';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    }else{
+        nextPlayer();
+    }
+
+
+    //Next Player    
+
+})
+
+function nextPlayer() {
+    
+    activePlayer=== 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    document.querySelector('.dice').style.display = 'none';
+}
 
 
 
